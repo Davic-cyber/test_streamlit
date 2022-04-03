0b43367416184312b9ceffea68e1c07add46b171
Davic-cyber <55009136+Davic-cyber@users.noreply.github.com>
2022-04-03 16:05:28 +0630

    Update data_visual.py

--- data_visual.py
+++ data_visual.py
@@ -1,6 +1,10 @@
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+#import streamlit as st
 
-xData=[10,20,30,40,50,60,70,80,90]
-yData=[100,200,300,400,500,600,700,800,900]
-plt.plot(xData,yData)
-plt.show()
+#xData=[10,20,30,40,50,60,70,80,90]
+#yData=[100,200,300,400,500,600,700,800,900]
+#plt.plot(xData,yData)
+#plt.show()
+
+import streamlit as st
+st.title("hello")
